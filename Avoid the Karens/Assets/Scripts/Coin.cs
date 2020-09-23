a2d52106c4abfeca6a6fddfe4a41a4ff96373566
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-09-23 15:21:07 +0100

    Have Coin Pick Up and Shopping list done
    just need to add all the shopping to the list that we want in the game and then tweek the code to fit. when the play hits the bread pick up the player will get two score and the bread will cross off on the list, when the player hits the mix veg both the carrots and the peas will be crossed off and the player will only get a score of one.

---
+++ Avoid the Karens/Assets/Scripts/Coin.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coin : MonoBehaviour
+{
+    public GameObject CoinSingle;
+    public GameObject CoinDouble;
+
+    public Image bread;
+    public Image peas;
+    public Image carrots;
+    public bool imageOff =  false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bread.gameObject.SetActive(false); 
+        peas.gameObject.SetActive(false);
+        carrots.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+       
+            if (gameObject == CoinDouble && !imageOff)
+            {
+
+            imageOff = true;
+            peas.gameObject.SetActive(true);
+            carrots.gameObject.SetActive(true);
+            Debug.Log("work");
+            Score.CoinAmount += 1;
+            Destroy(gameObject);
+        }
+
+         else
+       
+        if (gameObject == CoinSingle && !imageOff)
+        {
+
+            imageOff = true;
+            bread.gameObject.SetActive(true);
+            Debug.Log("work single");
+            Score.CoinAmount += 2;
+            Destroy(gameObject);
+        }
+    }
+}
