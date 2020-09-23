7c41e351d6b3fd1dbafbe4ff9d02d4c28fc75c92
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2020-09-23 13:53:26 +0100

    NPC Movement
    Set up NPC Movement between 2 points - J

---
+++ Avoid the Karens/Assets/Scripts/NPCController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+
+public class NPCController : MonoBehaviour {
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+
+        // Disable to make NPC move constantly
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+
+    void GotoNextPoint() {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Tells them where to go first
+        agent.destination = points[destPoint].position;
+
+        // Picks the next location, restarting the loop is the end is reached
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+    void Update () {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+}
