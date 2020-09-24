8b4d5cb64dde9714c8642b3fa806dc338b38061b
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2020-09-24 12:01:01 +0100

    Countdown Update
    Countdown n decreses faster based on proximity to Jeff and Ron - J

--- Avoid the Karens/Assets/Scripts/Collisions.cs
+++ Avoid the Karens/Assets/Scripts/Collisions.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Collisions : MonoBehaviour
 {
+    public bool sick = false;
+    
     void OnTriggerEnter(Collider col2)
     {
         if (col2.gameObject.tag == "Player")
         {
+            sick = true;
+            
             Debug.Log("I'm a Genius");
+        }
+        else
+        {
+            sick = false;
         }
     }
 }
--- Avoid the Karens/Assets/Scripts/UITimer.cs
+++ Avoid the Karens/Assets/Scripts/UITimer.cs
@@ -7,12 +7,31 @@ public class UITimer : MonoBehaviour
     public bool playing;
     private float Timer;
     public float timeRemaining = 10;
+    public Collisions Ron;
+    public Collisions Jeff;
 
-    void Update () {
-
+    /*void Start()
+    {
+        boob.sick = true;
+    }*/
+    
+    void Update ()
+    {
+        //isSick = Collisions.sick;
+        
         if (timeRemaining > 0)
         {
             timeRemaining -= Time.deltaTime;
+        }
+
+        if (timeRemaining > 0 && Ron.sick == true)
+        {
+            timeRemaining -= (Time.deltaTime) * 10;
+        }
+        
+        if (timeRemaining > 0 && Jeff.sick == true)
+        {
+            timeRemaining -= (Time.deltaTime) * 10;
         }
         
         if(playing == true)
