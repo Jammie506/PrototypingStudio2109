ba9b7dfcd17323186208b0013f1486b013026305
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2020-09-24 12:59:41 +0100

    Enemy Vision
    Enemies now have alterable Feilds of View that will prnt to the Log when the Player enters their feild of View - J

--- Avoid the Karens/Assets/Scripts/NPCController.cs
+++ Avoid the Karens/Assets/Scripts/NPCController.cs
@@ -8,6 +8,22 @@ public class NPCController : MonoBehaviour {
     public Transform[] points;
     private int destPoint = 0;
     private NavMeshAgent agent;
+    public float viewRadius;
+    [Range(0,306)]
+    public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obsticleMask;
+    public bool See = false;
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
 
     void Start () {
         agent = GetComponent<NavMeshAgent>();
@@ -17,9 +33,10 @@ public class NPCController : MonoBehaviour {
 
         GotoNextPoint();
         //OnCollisionEnter();
+
+        StartCoroutine("FindTargetsWithDelay", .2f);
     }
-
-
+    
     void GotoNextPoint() {
         // Returns if no points have been set up
         if (points.Length == 0)
@@ -45,5 +62,35 @@ public class NPCController : MonoBehaviour {
         {
             Debug.Log("Hi");
         }
+    }
+
+    void FindVisibleTargets()
+    {
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float distToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, dirToTarget, distToTarget, obsticleMask))
+                {
+                    See = true;
+                    Debug.Log("I See You");
+                }
+            }
+        }
+    }
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+        
     }
 }
