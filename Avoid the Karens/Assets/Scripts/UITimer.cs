2b2300cafffda2e9698f1bfc18a483e164e6cab1
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2020-09-24 11:33:05 +0100

    Basic Timer
    Timer is working to count down from a pblic float - J

---
+++ Avoid the Karens/Assets/Scripts/UITimer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UITimer : MonoBehaviour
+{	
+    public Text TimerText; 
+    public bool playing;
+    private float Timer;
+    public float timeRemaining = 10;
+
+    void Update () {
+
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        
+        if(playing == true)
+        {
+            TimerText.text = timeRemaining.ToString("00");
+        }
+    }
+
+}
