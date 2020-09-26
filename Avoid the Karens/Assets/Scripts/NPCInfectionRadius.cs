1b465e9b6dd855e7b26cc951ff27ff50331fc82a
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-09-26 17:03:04 +0100

    health done

--- Avoid the Karens/Assets/Scripts/NPCInfectionRadius.cs
+++ Avoid the Karens/Assets/Scripts/NPCInfectionRadius.cs
@@ -1,25 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class NPCInfectionRadius : MonoBehaviour
 {
+    public Image Infection;
 
+    
     public float EnemyRadius = 10f;
     Transform playerTarget;
+    //public Infec infec;
+
+    public const int InfectionMax = 100;
+    public const int InfectionLeast = 0;
+    public float InfectionAmount =0;
+    public float DecreaseAmount =5;
+    public float InfectionIncreaseAmount = 5;
+
+
+    public Image healthbar;
+    public const int maxHealth = 100;
+    //public static float health;
+
+   // public  float maxHealth = 100f;
+    //public static float health;
+    public  float HealthAmount = 0;
+    public float HealthDecreaseAmount = 1;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTarget = PlayerManager.instance.player.transform;
+        //healthbar = GetComponent<Image>();
+        HealthAmount = maxHealth;
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if
+        
+
+        float distance = Vector3.Distance(playerTarget.position, transform.position);
+        if(distance <= EnemyRadius)
+        {
+
+            // infec.Update();
+            InfectionAmount += InfectionIncreaseAmount * Time.deltaTime;
+            InfectionAmount = Mathf.Clamp(InfectionAmount, 0f, InfectionMax);
+           // Infection.fillAmount = infec.GetInfecNormalized();
+            Infection.fillAmount = InfectionAmount / InfectionMax;
+        }
+        else if (distance >= EnemyRadius)
+        {
+            InfectionAmount -= DecreaseAmount * Time.deltaTime;
+            InfectionAmount = Mathf.Clamp(InfectionAmount, 0f, InfectionLeast);
+            Infection.fillAmount = InfectionAmount / InfectionMax;
+        }
+
+        if (InfectionAmount >= InfectionMax)
+        {
+            HealthAmount -= HealthDecreaseAmount * Time.deltaTime;
+            //healthbar.fillAmount = health / maxHealth;
+            healthbar.fillAmount = HealthAmount / maxHealth;
+        }
     }
 
-     void OnDrawGizmosSelected()
+
+
+    void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, EnemyRadius);
