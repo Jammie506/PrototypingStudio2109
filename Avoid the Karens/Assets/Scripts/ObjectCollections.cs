b7fb5e7a55dc3007ff728647c5d6ed9b681af7d7
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2020-09-27 16:35:39 +0100

    PickUps
    Player can now collect items - J

---
+++ Avoid the Karens/Assets/Scripts/ObjectCollections.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollections : MonoBehaviour
+{
+     public bool picked = false;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            picked = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            picked = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider pickup)
+    {
+        /*if (pickup.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Ready to Collect");
+        }*/
+        if (picked && pickup.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collected");
+            
+            Destroy(gameObject);
+        }
+        
+    }
+}
