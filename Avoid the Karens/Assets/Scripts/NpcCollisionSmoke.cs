895f96a3d40f27d0b88ff51a4f6eec9e3e111d0d
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-09-28 20:26:35 +0100

    created particle effect and have it spwn when npc collides with anti masker

---
+++ Avoid the Karens/Assets/Scripts/NpcCollisionSmoke.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcCollisionSmoke : MonoBehaviour
+{
+    public Transform smoke;
+    public AudioSource AudioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //public void OnCollisionEnter(Collision collision)
+  //  {
+       // Instantiate(smoke, transform.position, transform.rotation);
+   // }
+    public void OnTriggerEnter(Collider other)
+    {
+        Instantiate(smoke, transform.position, transform.rotation);
+        AudioSource.Play();
+    }
+
+
+
+
+
+
+}
