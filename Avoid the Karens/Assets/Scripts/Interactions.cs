79b3d92f5efdaac4730dcccd5f470576266a1983
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2020-09-28 12:10:08 +0100

    Lunch

--- Avoid the Karens/Assets/Scripts/Interactions.cs
+++ Avoid the Karens/Assets/Scripts/Interactions.cs
@@ -2,29 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class Interactions : MonoBehaviour
 {
-    public GameObject Bread;
-    public GameObject Cheese;
+    public GameObject bread;
+    public GameObject cheese;
     
-    public ObjectCollections Shelf;
+     public ObjectCollections shelf;
     
     public Renderer bRend;
     public Renderer cRend;
     
     public Image breadHigh;
     public Image cheeseHigh;
+    public Text pickupControls;
 
     public bool picked;
     public bool gotBread;
     public bool gotCheese;
+    public bool on;
 
     private void Start()
     {
         breadHigh.gameObject.SetActive(false);
         cheeseHigh.gameObject.SetActive(false);
+        pickupControls.gameObject.SetActive(false);
 
         gotBread = false;
         gotCheese = false;
@@ -43,11 +47,11 @@ public class Interactions : MonoBehaviour
             picked = false;
         }
 
-        if (Shelf.droppedBread && gameObject == Bread)
+        if (shelf.droppedBread && gameObject == bread)
         {
             breadHigh.gameObject.SetActive(false);
         }
-        else if (Shelf.droppedCheese && gameObject == Cheese)
+        else if (shelf.droppedCheese && gameObject == cheese)
         {
             cheeseHigh.gameObject.SetActive(false);
         }
@@ -56,19 +60,35 @@ public class Interactions : MonoBehaviour
     //this is where items are collected, they are dropped in ObjectCollections
     public void OnTriggerStay(Collider enter)
     {
-        if (enter.gameObject.CompareTag("Player") && picked && gameObject == Bread && !gotBread)
+        if (enter.gameObject.CompareTag("Player"))
+        { 
+            on = true; 
+            pickupControls.gameObject.SetActive(true);
+        }
+        
+        if (enter.gameObject.CompareTag("Player") && picked && gameObject == bread && !gotBread)
         {
             Debug.Log("Transferred");
                 gotBread = true;
                 bRend.enabled = false;
                 breadHigh.gameObject.SetActive(true);
         }
-        else if (enter.gameObject.CompareTag("Player") && picked && gameObject == Cheese && !gotCheese)
+        else if (enter.gameObject.CompareTag("Player") && picked && gameObject == cheese && !gotCheese)
         {
             Debug.Log("Transferred");
                 gotCheese = true;
                 cRend.enabled = false;
                 cheeseHigh.gameObject.SetActive(true);
-        } 
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        { 
+            on = false; 
+            pickupControls.gameObject.SetActive(false);
+        }
+        
     }
 }
--- Avoid the Karens/Assets/Scripts/ObjectCollections.cs
+++ Avoid the Karens/Assets/Scripts/ObjectCollections.cs
@@ -2,25 +2,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class ObjectCollections : MonoBehaviour
 {
-    public GameObject Shelf;
+    public GameObject shelf;
     
-    public Image BreadCross;
-    public Image CheeseCross;
+    public Image breadCross;
+    public Image cheeseCross;
+    public Text dropControls;
 
     public Interactions interBread;
     public Interactions interCheese;
 
     public bool droppedBread;
     public bool droppedCheese;
+    public bool on;
 
     private void Start()
     {
-        BreadCross.gameObject.SetActive(false);
-        CheeseCross.gameObject.SetActive(false);
+        breadCross.gameObject.SetActive(false);
+        cheeseCross.gameObject.SetActive(false);
+        dropControls.gameObject.SetActive(false);
     }
 
     private void Update()
@@ -37,22 +41,42 @@ public class ObjectCollections : MonoBehaviour
 
     private void OnTriggerStay(Collider shelf)
     {
-        if (shelf.gameObject.CompareTag("Player") && interBread.picked && gameObject == Shelf && interBread.gotBread)
+        if (shelf.gameObject.CompareTag("Player") && interBread.picked && gameObject == this.shelf && interBread.gotBread)
         { 
             Debug.Log("Dropped");
             Score.CoinAmount += 1;
             interBread.gotBread = false;
             droppedBread = true;
-            BreadCross.gameObject.SetActive(true);
+            breadCross.gameObject.SetActive(true);
+            //dropControls.gameObject.SetActive(true);
             
         } 
-        else if (shelf.gameObject.CompareTag("Player") && interCheese.picked && gameObject == Shelf && interCheese.gotCheese)
+        else if (shelf.gameObject.CompareTag("Player") && interCheese.picked && gameObject == this.shelf && interCheese.gotCheese)
         { 
             Debug.Log("Dropped");
             Score.CoinAmount += 2;
             interCheese.gotCheese = false;
             droppedCheese = true;
-            CheeseCross.gameObject.SetActive(true);
+            cheeseCross.gameObject.SetActive(true);
+            //dropControls.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        { 
+            on = true; 
+            dropControls.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        { 
+            on = false; 
+            dropControls.gameObject.SetActive(false);
         }
     }
 }
